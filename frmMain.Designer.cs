a84ebcda5ea63c2c361adc1f75fe5598d760790b
Hoàng Hồng Phong <thieugiakk.hhp@gmail.com>
2021-08-17 20:24:15 +0700

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Lab02_04
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSoTaiKhoan = new System.Windows.Forms.Label();
+            this.lblTenKhachHang = new System.Windows.Forms.Label();
+            this.lblDiaChiKhachHang = new System.Windows.Forms.Label();
+            this.lblSoTienTrongTaiKhoan = new System.Windows.Forms.Label();
+            this.txtSoTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtTenKhachHang = new System.Windows.Forms.TextBox();
+            this.txtDiaChiKhachHang = new System.Windows.Forms.TextBox();
+            this.txtSoTienTrongTaiKhoan = new System.Windows.Forms.TextBox();
+            this.btnThemCapNhat = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblTongTien = new System.Windows.Forms.Label();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.lvTTTK = new System.Windows.Forms.ListView();
+            this.clhSoTien = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clhDiaChi = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clhTenKhachHang = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clhSoTaiKhoan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clhSTT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(184, 26);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(606, 42);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "QUẢN LÝ THÔNG TIN TÀI KHOẢN";
+            // 
+            // lblSoTaiKhoan
+            // 
+            this.lblSoTaiKhoan.AutoSize = true;
+            this.lblSoTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoTaiKhoan.Location = new System.Drawing.Point(100, 115);
+            this.lblSoTaiKhoan.Name = "lblSoTaiKhoan";
+            this.lblSoTaiKhoan.Size = new System.Drawing.Size(101, 20);
+            this.lblSoTaiKhoan.TabIndex = 1;
+            this.lblSoTaiKhoan.Text = "Số tài khoản";
+            // 
+            // lblTenKhachHang
+            // 
+            this.lblTenKhachHang.AutoSize = true;
+            this.lblTenKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenKhachHang.Location = new System.Drawing.Point(100, 148);
+            this.lblTenKhachHang.Name = "lblTenKhachHang";
+            this.lblTenKhachHang.Size = new System.Drawing.Size(127, 20);
+            this.lblTenKhachHang.TabIndex = 1;
+            this.lblTenKhachHang.Text = "Tên khách hàng";
+            // 
+            // lblDiaChiKhachHang
+            // 
+            this.lblDiaChiKhachHang.AutoSize = true;
+            this.lblDiaChiKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaChiKhachHang.Location = new System.Drawing.Point(100, 181);
+            this.lblDiaChiKhachHang.Name = "lblDiaChiKhachHang";
+            this.lblDiaChiKhachHang.Size = new System.Drawing.Size(151, 20);
+            this.lblDiaChiKhachHang.TabIndex = 1;
+            this.lblDiaChiKhachHang.Text = "Địa chỉ khách hàng";
+            // 
+            // lblSoTienTrongTaiKhoan
+            // 
+            this.lblSoTienTrongTaiKhoan.AutoSize = true;
+            this.lblSoTienTrongTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoTienTrongTaiKhoan.Location = new System.Drawing.Point(100, 214);
+            this.lblSoTienTrongTaiKhoan.Name = "lblSoTienTrongTaiKhoan";
+            this.lblSoTienTrongTaiKhoan.Size = new System.Drawing.Size(176, 20);
+            this.lblSoTienTrongTaiKhoan.TabIndex = 1;
+            this.lblSoTienTrongTaiKhoan.Text = "Số tiền trong tài khoản";
+            // 
+            // txtSoTaiKhoan
+            // 
+            this.txtSoTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoTaiKhoan.Location = new System.Drawing.Point(350, 112);
+            this.txtSoTaiKhoan.Name = "txtSoTaiKhoan";
+            this.txtSoTaiKhoan.Size = new System.Drawing.Size(500, 27);
+            this.txtSoTaiKhoan.TabIndex = 1;
+            // 
+            // txtTenKhachHang
+            // 
+            this.txtTenKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenKhachHang.Location = new System.Drawing.Point(350, 145);
+            this.txtTenKhachHang.Name = "txtTenKhachHang";
+            this.txtTenKhachHang.Size = new System.Drawing.Size(500, 27);
+            this.txtTenKhachHang.TabIndex = 2;
+            // 
+            // txtDiaChiKhachHang
+            // 
+            this.txtDiaChiKhachHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiaChiKhachHang.Location = new System.Drawing.Point(350, 178);
+            this.txtDiaChiKhachHang.Name = "txtDiaChiKhachHang";
+            this.txtDiaChiKhachHang.Size = new System.Drawing.Size(500, 27);
+            this.txtDiaChiKhachHang.TabIndex = 3;
+            // 
+            // txtSoTienTrongTaiKhoan
+            // 
+            this.txtSoTienTrongTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoTienTrongTaiKhoan.Location = new System.Drawing.Point(350, 211);
+            this.txtSoTienTrongTaiKhoan.Name = "txtSoTienTrongTaiKhoan";
+            this.txtSoTienTrongTaiKhoan.Size = new System.Drawing.Size(500, 27);
+            this.txtSoTienTrongTaiKhoan.TabIndex = 4;
+            // 
+            // btnThemCapNhat
+            // 
+            this.btnThemCapNhat.Location = new System.Drawing.Point(513, 250);
+            this.btnThemCapNhat.Name = "btnThemCapNhat";
+            this.btnThemCapNhat.Size = new System.Drawing.Size(125, 30);
+            this.btnThemCapNhat.TabIndex = 5;
+            this.btnThemCapNhat.Text = "Thêm / Cập nhật";
+            this.btnThemCapNhat.UseVisualStyleBackColor = true;
+            this.btnThemCapNhat.Click += new System.EventHandler(this.btnThemCapNhat_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(644, 250);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(100, 30);
+            this.btnXoa.TabIndex = 6;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(750, 250);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 30);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblTongTien
+            // 
+            this.lblTongTien.AutoSize = true;
+            this.lblTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTongTien.Location = new System.Drawing.Point(527, 604);
+            this.lblTongTien.Name = "lblTongTien";
+            this.lblTongTien.Size = new System.Drawing.Size(67, 16);
+            this.lblTongTien.TabIndex = 1;
+            this.lblTongTien.Text = "Tổng tiền:";
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Enabled = false;
+            this.txtTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTongTien.Location = new System.Drawing.Point(600, 601);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(250, 22);
+            this.txtTongTien.TabIndex = 7;
+            // 
+            // lvTTTK
+            // 
+            this.lvTTTK.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clhSTT,
+            this.clhSoTaiKhoan,
+            this.clhTenKhachHang,
+            this.clhDiaChi,
+            this.clhSoTien});
+            this.lvTTTK.FullRowSelect = true;
+            this.lvTTTK.GridLines = true;
+            this.lvTTTK.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvTTTK.HideSelection = false;
+            this.lvTTTK.Location = new System.Drawing.Point(104, 290);
+            this.lvTTTK.Name = "lvTTTK";
+            this.lvTTTK.Size = new System.Drawing.Size(746, 309);
+            this.lvTTTK.TabIndex = 6;
+            this.lvTTTK.UseCompatibleStateImageBehavior = false;
+            this.lvTTTK.View = System.Windows.Forms.View.Details;
+            this.lvTTTK.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lvTTTK_ItemSelectionChanged);
+            // 
+            // clhSoTien
+            // 
+            this.clhSoTien.Text = "Số Tiền";
+            this.clhSoTien.Width = 100;
+            // 
+            // clhDiaChi
+            // 
+            this.clhDiaChi.Text = "Địa Chỉ";
+            this.clhDiaChi.Width = 175;
+            // 
+            // clhTenKhachHang
+            // 
+            this.clhTenKhachHang.Text = "Tên Khách Hàng";
+            this.clhTenKhachHang.Width = 200;
+            // 
+            // clhSoTaiKhoan
+            // 
+            this.clhSoTaiKhoan.Text = "Số Tài Khoản";
+            this.clhSoTaiKhoan.Width = 175;
+            // 
+            // clhSTT
+            // 
+            this.clhSTT.Text = "STT";
+            this.clhSTT.Width = 100;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 636);
+            this.Controls.Add(this.lvTTTK);
+            this.Controls.Add(this.txtTongTien);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThemCapNhat);
+            this.Controls.Add(this.txtSoTienTrongTaiKhoan);
+            this.Controls.Add(this.txtDiaChiKhachHang);
+            this.Controls.Add(this.txtTenKhachHang);
+            this.Controls.Add(this.txtSoTaiKhoan);
+            this.Controls.Add(this.lblTongTien);
+            this.Controls.Add(this.lblSoTienTrongTaiKhoan);
+            this.Controls.Add(this.lblDiaChiKhachHang);
+            this.Controls.Add(this.lblTenKhachHang);
+            this.Controls.Add(this.lblSoTaiKhoan);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.Text = "Quản lý tài khoản";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSoTaiKhoan;
+        private System.Windows.Forms.Label lblTenKhachHang;
+        private System.Windows.Forms.Label lblDiaChiKhachHang;
+        private System.Windows.Forms.Label lblSoTienTrongTaiKhoan;
+        private System.Windows.Forms.TextBox txtSoTaiKhoan;
+        private System.Windows.Forms.TextBox txtTenKhachHang;
+        private System.Windows.Forms.TextBox txtDiaChiKhachHang;
+        private System.Windows.Forms.TextBox txtSoTienTrongTaiKhoan;
+        private System.Windows.Forms.Button btnThemCapNhat;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblTongTien;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.ListView lvTTTK;
+        private System.Windows.Forms.ColumnHeader clhSTT;
+        private System.Windows.Forms.ColumnHeader clhSoTaiKhoan;
+        private System.Windows.Forms.ColumnHeader clhTenKhachHang;
+        private System.Windows.Forms.ColumnHeader clhDiaChi;
+        private System.Windows.Forms.ColumnHeader clhSoTien;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_04
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public List<ListViewItem> items;
+
+        private void ResetValue()
+        {
+            txtSoTaiKhoan.Text = "";
+            txtTenKhachHang.Text = "";
+            txtDiaChiKhachHang.Text = "";
+            txtSoTienTrongTaiKhoan.Text = "";
+        }
+
+        private int GetSelectedRow(string stk)
+        {
+            for (int i = 0; i < lvTTTK.Items.Count; i++)
+            {
+                if (lvTTTK.Items[i].SubItems[1].Text == stk)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void btnThemCapNhat_Click(object sender, EventArgs e)
+        {
+            if (txtSoTaiKhoan.Text == "" || txtTenKhachHang.Text == "" || txtDiaChiKhachHang.Text == "" || txtSoTienTrongTaiKhoan.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int selectedRow = GetSelectedRow(txtSoTaiKhoan.Text);
+                if (selectedRow == -1)
+                {
+                    ThemItem();
+
+                    MessageBox.Show("Thêm mới dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    CapNhatItem(selectedRow);
+
+                    MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void ThemItem()
+        {
+            int i = lvTTTK.Items.Count;
+
+            lvTTTK.Items.Add((i + 1).ToString());
+            lvTTTK.Items[i].SubItems.Add(txtSoTaiKhoan.Text);
+            lvTTTK.Items[i].SubItems.Add(txtTenKhachHang.Text);
+            lvTTTK.Items[i].SubItems.Add(txtDiaChiKhachHang.Text);
+            lvTTTK.Items[i].SubItems.Add(txtSoTienTrongTaiKhoan.Text);;
+
+            i++;
+
+            ResetValue();
+
+            CapNhatTongTien();
+        }
+
+        private void CapNhatItem(int selectedRow)
+        {
+            lvTTTK.Items.RemoveAt(selectedRow);
+
+            lvTTTK.Items.Insert(selectedRow, (selectedRow + 1).ToString());
+            lvTTTK.Items[selectedRow].SubItems.Add(txtSoTaiKhoan.Text);
+            lvTTTK.Items[selectedRow].SubItems.Add(txtTenKhachHang.Text);
+            lvTTTK.Items[selectedRow].SubItems.Add(txtDiaChiKhachHang.Text);
+            lvTTTK.Items[selectedRow].SubItems.Add(txtSoTienTrongTaiKhoan.Text);
+
+            ResetValue();
+
+            CapNhatTongTien();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lvTTTK_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            txtSoTaiKhoan.Text = e.Item.SubItems[1].Text;
+            txtTenKhachHang.Text = e.Item.SubItems[2].Text;
+            txtDiaChiKhachHang.Text = e.Item.SubItems[3].Text;
+            txtSoTienTrongTaiKhoan.Text = e.Item.SubItems[4].Text;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            int selectedRow = GetSelectedRow(txtSoTaiKhoan.Text);
+            if (MessageBox.Show("Xóa dữ liệu thành công!", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                lvTTTK.Items.RemoveAt(selectedRow);
+            }
+
+            ResetValue();
+
+            CapNhatTongTien();
+        }
+
+        private void CapNhatTongTien()
+        {
+            long tongTien = 0;
+
+            for (int i = 0; i < lvTTTK.Items.Count; i++)
+            {
+                tongTien += long.Parse(lvTTTK.Items[i].SubItems[4].Text);
+            }
+
+            txtTongTien.Text = tongTien.ToString();
+        }
+    }
+}
